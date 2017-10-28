e8e0b5bcee1f5c10b8a680a1bbb595aa7062cd78
Zach Connelly <zmconnelly@gmail.com>
2017-10-27 18:00:06 -0600

    ES

--- src/ESNetworkTrainer.java
+++ src/ESNetworkTrainer.java
@@ -2,12 +2,24 @@ import java.util.List;
 
 public class ESNetworkTrainer extends NetworkTrainerBase {
 
-    ESNetworkTrainer() {
+    private final int populationSize;
+    private final int numOffspring;
 
+    ESNetworkTrainer(int populationSize, int numOffspring) {
+        this.populationSize = populationSize;
+        this.numOffspring = numOffspring;
     }
 
     @Override
     public INeuralNetwork train(INeuralNetwork network, List<Sample> samples) {
+        // Generate initial population randomly
+        // Do
+            // Cross
+            // Mutate
+            // Evaluate
+            // Choose new population
+        // While some condition
+
         return network;
     }
 }
