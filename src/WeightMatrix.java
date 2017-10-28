8cdadc71ce2e5005bfabaef0e309b76a61bacf28
Zach Connelly <zmconnelly@gmail.com>
2017-10-27 18:02:10 -0600

    WIP Serialization

--- src/WeightMatrix.java
+++ src/WeightMatrix.java
@@ -1,4 +1,8 @@
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class WeightMatrix {
 
@@ -6,20 +10,44 @@ public class WeightMatrix {
     private List<Integer> dimmensions;
 
     private INeuralNetwork network;
+    private final int networkSize;
 
 
     public WeightMatrix(INeuralNetwork network) {
         this.network = network;
-        int numLayers = network.getSize();
+        this.networkSize = network.getSize();
+        this.weights = new ArrayList<>();
 
-        for (int i = 0; i < numLayers; i++) {
+        for (int i = 0; i < networkSize; i++) {
             Layer layer = network.getLayer(i);
+            this.dimmensions.add(layer.size);
+            this.weights.addAll(layer.getNeurons()
+                    .stream()
+                    .map(Neuron::getWeights)
+                    .flatMap(Collection::stream)
+                    .collect(Collectors.toList())
+            );
         }
     }
 
 
-    public INeuralNetwork buildNetwork() {
-        // Construct and set new layers with current weights
+    public INeuralNetwork buildNetwork(Optional<List<Double>> weightMatrix) {
+        for (int i = 0; i < networkSize ; i++) {
+            Layer
+        }
+
         return this.network;
+    }
+
+    public List<Double> getWeights() {
+        return this.weights;
+    }
+
+    public void setWeights(List<Double> weights) {
+        if (weights.size() != this.weights.size()) {
+            throw new IllegalArgumentException("Invalid weight matrix");
+        }
+
+        this.weights = weights;
     }
 }
