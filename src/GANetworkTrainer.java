fe41eda9d119a31e3b4a8ba29fdad5c8d85fef29
Keely Weisbeck <keelyweisbeck@gmail.com>
2017-10-29 18:06:24 -0600

    GA network outline

--- src/GANetworkTrainer.java
+++ src/GANetworkTrainer.java
@@ -8,6 +8,32 @@ public class GANetworkTrainer extends NetworkTrainerBase {
 
     @Override
     public INeuralNetwork train(INeuralNetwork network, List<Sample> samples) {
+        //t = 0;
+        //initialize population
+        //fitness function
+        //while not converge
+            // select parent
+            //apply cross over
+            //apply mutation
+            //evaluate fitness of mutation result
+            //replace child into population
+        // end while
+
+        //return best
         return network;
+    }
+
+    private void crossover(parent1, parent2) {
+    }
+
+    private void mutation(parent) {
+    }
+
+    private void fitnessFunction(population[]) {
+        int fitness = 0;
+
+        for(int i = 0; i < population.size(); i++) {
+
+        }
     }
 }
